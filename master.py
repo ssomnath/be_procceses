a3c9e33234a816bef8bb50f4cebb4673ab913797
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-08-14 18:55:54 -0400

    script now only prints on 0th rank

--- master.py
+++ master.py
@@ -11,28 +11,36 @@ def main(input_data_path):
     MPI = usid.comp_utils.get_MPI()
 
     h5_kwargs = {'mode': 'r+'}
+    mpi_rank = 0
+
     if MPI is not None:
+        mpi_rank = MPI.COMM_WORLD.Get_rank()
         print('*** Master script called using mpirun ***')
         h5_kwargs.update({'driver': 'mpio', 'comm': MPI.COMM_WORLD})
 
     h5_f = h5py.File(input_data_path, **h5_kwargs)
     
     h5_main = h5_f['Measurement_000/Channel_000/Raw_Data']
-    print(h5_main)
 
-    usid.hdf_utils.print_tree(h5_f)
+    if mpi_rank == 0:
+        print(h5_main)
+        usid.hdf_utils.print_tree(h5_f)
 
     proc = BESHOfitter(h5_main, verbose=True)
 
     proc.set_up_guess()
     h5_guess = proc.do_guess()
-    print(h5_guess)
+
+    if mpi_rank == 0:
+        print(h5_guess)
+        usid.hdf_utils.print_tree(h5_f)
 
     proc.set_up_fit()
     h5_fit = proc.do_fit()
-    print(h5_fit)
 
-    usid.hdf_utils.print_tree(h5_f)
+    if mpi_rank == 0:
+        print(h5_fit)
+        usid.hdf_utils.print_tree(h5_f)
 
     h5_f.close()
 
