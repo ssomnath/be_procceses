aa26b8c7adc87b733fd7a842ec48bf94593eb914
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-08-14 15:48:14 -0400

    Single script for MPI / joblib computation. Minor tidying up

--- master.py
+++ master.py
@@ -1,24 +1,21 @@
 import sys
-# sys.path.append(r'C:\Users\Suhas\PycharmProjects\pyUSID')
-# sys.path.append(r'C:\Users\Suhas\PycharmProjects\pycroscopy')
-import os
 import h5py
-import shutil
-import numpy as np
 import pyUSID as usid
 
-def main(orig_data_path):
-    
-    from be_sho_fitter import BESHOfitter
+from be_sho_fitter import BESHOfitter
 
-    # orig_data_path = r'C:\Users\Suhas\PycharmProjects\pyUSID\data\BEPS_small.h5'
+
+def main(input_data_path):
 
     # Dynamically change mode etc.
-    mpi_run = False
+    MPI = usid.comp_utils.get_MPI()
+
     h5_kwargs = {'mode': 'r+'}
-    if mpi_run:
-        h5_kwargs = {}
-    h5_f = h5py.File(input_data_path, **kwargs)
+    if MPI is not None:
+        print('*** Master script called using mpirun ***')
+        h5_kwargs.update({'driver': 'mpio', 'comm': MPI.COMM_WORLD})
+
+    h5_f = h5py.File(input_data_path, **h5_kwargs)
     
     h5_main = h5_f['Measurement_000/Channel_000/Raw_Data']
     print(h5_main)
@@ -26,14 +23,19 @@ def main(orig_data_path):
     usid.hdf_utils.print_tree(h5_f)
 
     proc = BESHOfitter(h5_main, verbose=True)
+
     proc.set_up_guess()
     h5_guess = proc.do_guess()
+    print(h5_guess)
 
     proc.set_up_fit()
-    proc.do_fit()
+    h5_fit = proc.do_fit()
+    print(h5_fit)
+
     usid.hdf_utils.print_tree(h5_f)
 
     h5_f.close()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
